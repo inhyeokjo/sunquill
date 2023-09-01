d8abcb21cafae6d33957c6f13a87bb28057a4992
inhyeok <alzpqm@naver.com>
2023-09-01 16:59:07 +0900

    feat: article DB table 추가

---
+++ src/main/resources/db/migration/common/V0_0_001__create_article_model.sql
@@ -0,0 +1,12 @@
+CREATE TABLE article
+(
+    article_id    BIGINT PRIMARY KEY AUTO_INCREMENT,
+    title         VARCHAR(255),
+    picture_url   VARCHAR(255),
+    category      VARCHAR(255),
+    contents      LONGTEXT,
+    invisible     BIT,
+    author_id     BIGINT,
+    modified_date DATETIME,
+    created_date  DATETIME
+)
