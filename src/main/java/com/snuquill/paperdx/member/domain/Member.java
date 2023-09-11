fa4ee291e86284d83b571e5a5c04e9106c081600
inhyeok <alzpqm@naver.com>
2023-09-11 22:05:16 +0900

    feat: member 도메인 모델 구현

---
+++ src/main/java/com/snuquill/paperdx/member/domain/Member.java
@@ -0,0 +1,29 @@
+package com.snuquill.paperdx.member.domain;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "member_id", nullable = false)
+	private Long id;
+	private String name;
+	@Embedded
+	@AttributeOverride(name = "url", column = @Column(name = "profile_picture_url"))
+	private Picture profilePicture;
+	private String email;
+	private String role;
+	private String phone;
+}
---
+++ src/main/java/com/snuquill/paperdx/member/domain/Picture.java
@@ -0,0 +1,16 @@
+package com.snuquill.paperdx.member.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class Picture {
+
+	private String url;
+}
