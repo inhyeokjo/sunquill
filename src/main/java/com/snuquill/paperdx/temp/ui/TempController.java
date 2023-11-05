97cc8011fa3e15fc93434d08ab261214d42cf18c
inhyeok <alzpqm@naver.com>
2023-11-06 03:52:23 +0900

    refactor: change temporally controller position

--- src/main/java/com/snuquill/paperdx/temp/ui/TempController.java
+++ src/main/java/com/snuquill/paperdx/temp/ui/TempController.java
@@ -1,11 +1,11 @@
-package com.snuquill.paperdx.article.ui;
+package com.snuquill.paperdx.temp.ui;
 
-import com.snuquill.paperdx.article.application.ArticleDetailService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/temp")
@@ -13,12 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Validated
 public class TempController {
 
-	private final ArticleDetailService articleDetailService;
-
 	@GetMapping("/home")
 	public String getHomepage() {
 		return "home";
 	}
+
 	@GetMapping("/404")
 	public String get404() {
 		return "404";
