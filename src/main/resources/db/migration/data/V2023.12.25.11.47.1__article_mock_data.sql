f80430ea89e2963062f8978ea0c7e0edda7d55a6
inhyeok <alzpqm@naver.com>
2023-12-25 19:27:54 +0900

    feat: add mock data

---
+++ src/main/resources/db/migration/data/V2023.12.25.11.47.1__article_mock_data.sql
@@ -0,0 +1,128 @@
+INSERT INTO article (title, picture_url, category, contents, invisible, author_id, modified_date, created_date)
+VALUES ('Fire at apartment building in northern Seoul kills 2, injures 29 people',
+        'https://picsum.photos/1080/494', 'FEATURES',
+        'A fire broke out at an apartment building in northern Seoul, leaving two people dead and injuring 29 people, firefighters said Monday.
+
+The blaze occurred at the third floor of the 27-story apartment building in Seoul''s Dobong district at around 4:57 a.m., according to fire services.
+
+Two men in their 30s and a woman in her 70s were found in a state of cardiac arrest and sent to a hospital, and two of them were pronounced dead.
+
+About 222 firefighters battled the fire and extinguished it about three hours after the fire started.
+
+Police and firefighters are investigating to determine the exact cause of the accident. (Yonhap)', false, 1, NOW(), NOW());
+
+INSERT INTO article (title, picture_url, category, contents, invisible, author_id, modified_date, created_date)
+VALUES ('Pope laments war in Holy Land on solemn Christmas Eve',
+        'https://picsum.photos/1080/494', 'FEATURES',
+        'Pope Francis lamented war in the land of the birth of Jesus on Sunday, where Christmas Eve brought only fresh bloodshed and an intensification of fighting across the length of the Gaza Strip.
+
+Hours before midnight tolled for Christmas in the Holy Land, Palestinian health officials reported at least 70 dead in an Israeli airstrike on central Gaza. The Israeli military spokesperson''s office said it was looking into the report.
+
+Israel reported the highest two-day death toll for its troops in more than a month.
+
+The head of the Islamic Jihad militant group joined talks in Cairo, one small sign at least that diplomacy remained alive.
+
+But in Bethlehem, the Israeli-occupied Palestinian West Bank city where Jesus Christ is believed to have been born in a stable 2,000 years ago, clergy cancelled traditional celebrations for the first time in memory.
+
+"Tonight, our hearts are in Bethlehem, where the Prince of Peace is once more rejected by the futile logic of war, by the clash of arms that even today prevents him from finding room in the world," Pope Francis said, presiding at Christmas Eve Mass in St. Peter''s Basilica in Rome.
+
+Palestinian Christians earlier held a Christmas vigil in Bethlehem with candle-lit hymns and prayers for peace in Gaza instead of the usual celebrations.
+
+There was no large tree, the usual centerpiece of Bethlehem''s Christmas celebrations. Nativity figurines in churches were placed amid rubble and barbed wire in solidarity with the people of Gaza.
+
+In the latest massive airstrike, health ministry spokesperson Ashraf Al-Qidra said at least 70 Palestinians were killed and several houses damaged in central Gaza''s Maghazi area on Sunday night. He said many were women and children.
+
+Since a week-long truce collapsed at the start of the month, fighting has only intensified on the ground, with war spreading from the north of the Gaza Strip to the full length of the enclave.',
+        false, 1, NOW(), NOW());
+
+
+INSERT INTO article (title, picture_url, category, contents, invisible, author_id, modified_date, created_date)
+VALUES ('Incheon''s child-rearing support plan sparks controversy',
+        'https://picsum.photos/1080/494', 'FEATURES',
+        'Incheon’s child-rearing support plan to provide a total of 100 million won ($76,746) to all children born in the port city until they turn 18 is igniting controversy and mixed reactions. The plan is poised to start next year.
+
+According to Incheon Metropolitan City, it has decided to add 28 million won from its own budget to the existing 72 million won from the central and local governments. To this end, the city has allocated 40.4 billion won for the policy.
+
+Previously, financial assistance comprised 1 million won for pregnancy and related medical expenses, 2 million won upon childbirth, 18 million won for parental benefits, 9.6 million won per month for child benefits, 25.4 million won for child care and meals and 16.5 million won for elementary, middle and high school education.
+
+Through the expanded plan, the city intends to allocate an additional 500,000 won for pregnant women''s transportation, 100,000 won per month for seven years starting from a child''s first birthday, and a total of 19.8 million won, equating to 150,000 won per month from ages 8 to 18.
+
+Following the city government''s announcement, there were positive responses to the plan, with some expressing optimism that it would contribute to a boost in the birthrate.
+
+“These days, it costs a lot of money to raise a child, so I think Incheon’s policy to support more expenses definitely benefits people,” said an office worker surnamed Kim who works in Incheon.
+
+While the announcement garnered positive reactions from some like Kim, concerns also have been raised, with some questioning its efficacy and worrying about a possible strain on the city''s budget.
+
+“I doubt this policy can prompt a sudden change in the decisions of young couples who are not inclined to have children,” said Lee Jae-hye, a 30-year-old office worker in Incheon.
+
+“Instead of ensuring future benefits through subsidies, my concern lies in the potential reduction of citizens'' tax burdens or other local currency subsidies.”
+
+Kang Young-sook, a teacher of nursing who is in her 50s, emphasized that economic support alone cannot address the complex challenges of child-rearing.
+
+“In my opinion, the crucial element is ensuring parents have the time to spend with their children,” she said.
+
+She sees that rather than merely increasing financial support, it appears more effective to enable parents of young children to adjust their work schedules with greater flexibility.
+
+Drawing from examples observed in the facility where she works, Kang highlighted the cases of children arriving early in the morning, which she attributes to the prevalence of dual-income households.
+
+“Some children arrive as early as 7:30 a.m. and return home around 7 p.m.,” Kang said, underscoring the potential for emotional anxiety in these children.
+
+Suh Young-sook, an honorary professor specializing in Child Welfare and Studies at Sookmyung Women''s University, suggested that targeted support might be more effective than just increasing the total support expenses.
+
+She proposed focusing intensively on the critical early years with economic support such as providing subsidies, and investing in robust parenting support and education systems when parents return to work.
+
+“Effectively encouraging people to have children requires the creation of differentiated policies that parents perceive as genuinely helpful in raising children,” Suh said.',
+        false, 1, NOW(), NOW());
+
+
+INSERT INTO article (title, picture_url, category, contents, invisible, author_id, modified_date, created_date)
+VALUES ('Israeli airstrikes kill dozens, Gaza officials say, in Christmas bloodshed',
+        'https://picsum.photos/1080/494', 'FEATURES',
+        'Pope Francis lamented the war in the Holy Land where Palestinian health officials said airstrikes killed at least 78 people on Christmas Eve in one of the Gaza Strip''s deadliest nights in Israel''s 11-week-old battle with Hamas.
+
+Israeli strikes that began hours before midnight persisted into Christmas Day on Monday. Local residents and Palestinian media said Israel stepped up air and ground shelling against al-Bureij in central Gaza.
+
+At least 70 people were killed in an Israeli airstrike targeting Maghazi in central Gaza, health ministry spokesperson Ashraf Al-Qidra said, adding that many were women and children.
+
+The Israeli army said it was reviewing the report of a Maghazi incident and was committed to minimizing harm to civilians. Hamas denies the Israeli charge that it operates in densely populated areas or uses civilians as human shields.
+
+The Palestinian Red Crescent published footage of the wounded being transported to hospitals. It said Israeli warplanes were bombing main roads between central Gaza, hindering the passage of ambulances and emergency vehicles.
+
+Medics said a separate Israeli airstrike in Khan Younis in southern Gaza killed eight Palestinians.
+
+Clergy cancelled celebrations in Bethlehem, the Israeli-occupied Palestinian West Bank city where tradition has it that Jesus was born in a stable 2,000 years ago.
+
+"Tonight, our hearts are in Bethlehem, where the Prince of Peace is once more rejected by the futile logic of war, by the clash of arms that even today prevents him from finding room in the world," Pope Francis said, presiding at Christmas Eve Mass in St. Peter''s Basilica in Rome.
+
+Palestinian Christians earlier held a Christmas vigil in Bethlehem with candle-lit hymns and prayers for peace in Gaza instead of the usual celebrations.
+
+There was no large tree, the usual centerpiece of Bethlehem''s Christmas celebrations. Nativity figurines in churches were placed amid rubble and barbed wire in solidarity with the people of Gaza.',
+        false, 1, NOW(), NOW());
+
+
+
+INSERT INTO article (title, picture_url, category, contents, invisible, author_id, modified_date, created_date)
+VALUES ('N. Korea set to convene plenary meeting of ruling party this week',
+        'https://picsum.photos/1080/494', 'FEATURES',
+        'North Korea is set to hold a key party meeting this week amid expectations that its leader Kim Jong-un will likely reaffirm a hard-line stance against the United States and unveil plans to strengthen its military capabilities.
+
+The North''s state media earlier reported the country will convene the ninth plenary meeting of the eighth central committee of the ruling Workers'' Party late in December to review this year''s state policies and set policy goals for next year. It did not elaborate on when the meeting would be held.
+
+In recent years, the North''s leader has used a year-end party plenary meeting as a venue to deliver his major speech to replace his annual New Year''s Day address.
+
+Experts said Kim is widely expected to use the upcoming meeting to deliver a hostile message against the U.S. and South Korea and lay out a set of measures to bolster its military capabilities, including a detailed plan to launch additional military spy satellites.
+
+North Korea has vowed to strengthen solidarity with countries standing against the U.S. and is seeking to bolster its ties with China, the North''s main economic benefactor, and Russia amid strengthening security cooperation among Seoul, Washington and Tokyo.
+
+In particular, North Korea has been bolstering its military cooperation with Russia this year following the summit between Kim and Russian President Vladimir Putin in September.
+
+The U.S. and South Korea suspected that Pyongyang has supplied artillery and munitions to Moscow for use in Russia''s war in Ukraine. In return, North Korea might have received technical assistance from Russia for its launch of a military spy satellite.
+
+Last month, North Korea put a military spy satellite into orbit after two failed attempts in May and August. North Korea said it will launch more of such satellites in a short span of time and decide on a spy satellite launch plan for 2024 at the upcoming key party meeting.
+
+Last year, North Korea laid out 12 major economic goals for this year, placing top priority on boosting grain production amid the country''s food shortages. (Yonhap)',
+        false, 1, NOW(), NOW());
+
+
+
+
---
+++ src/main/resources/db/migration/data/V2023.12.25.14.30.1__video_info_mock_data.sql
@@ -0,0 +1,24 @@
+insert into video_info (iframe_src, title, upload_date, modified_date, created_date)
+values ('https://www.youtube.com/embed/9bZkp7q19f0', 'PSY - GANGNAM STYLE(강남스타일) M/V', DATE_SUB(NOW(), INTERVAL 10 DAY), DATE_SUB(NOW(), INTERVAL 10 DAY), DATE_SUB(NOW(), INTERVAL 10 DAY));
+
+insert into video_info (iframe_src, title, upload_date, modified_date, created_date)
+values ('https://www.youtube.com/embed/VShvFN6K_gE', '선우정아(SWJA)- 고양이 (Feat. 아이유) [가사/lyrics]', DATE_SUB(NOW(), INTERVAL 9 DAY), DATE_SUB(NOW(), INTERVAL 9 DAY), DATE_SUB(NOW(), INTERVAL 9 DAY));
+
+insert into video_info (iframe_src, title, upload_date, modified_date, created_date)
+values ('https://www.youtube.com/embed/9bZkp7q19f0', 'AKMU - ‘Love Lee’ M/V', DATE_SUB(NOW(), INTERVAL 8 DAY), DATE_SUB(NOW(), INTERVAL 8 DAY), DATE_SUB(NOW(), INTERVAL 8 DAY));
+
+insert into video_info (iframe_src, title, upload_date, modified_date, created_date)
+values ('https://www.youtube.com/embed/D8VEhcPeSlc', 'aespa 에스파 ''Drama'' MV', DATE_SUB(NOW(), INTERVAL 7 DAY), DATE_SUB(NOW(), INTERVAL 7 DAY), DATE_SUB(NOW(), INTERVAL 7 DAY));
+
+insert into video_info (iframe_src, title, upload_date, modified_date, created_date)
+values ('https://www.youtube.com/embed/jOTfBlKSQYY', 'NewJeans (뉴진스) ''ETA'' Official MV', DATE_SUB(NOW(), INTERVAL 6 DAY), DATE_SUB(NOW(), INTERVAL 6 DAY), DATE_SUB(NOW(), INTERVAL 6 DAY));
+
+insert into video_info (iframe_src, title, upload_date, modified_date, created_date)
+values ('https://www.youtube.com/embed/ArmDp-zijuc', 'NewJeans (뉴진스) ''Super Shy'' Official MV', DATE_SUB(NOW(), INTERVAL 5 DAY), DATE_SUB(NOW(), INTERVAL 5 DAY), DATE_SUB(NOW(), INTERVAL 5 DAY));
+
+insert into video_info (iframe_src, title, upload_date, modified_date, created_date)
+values ('https://www.youtube.com/embed/fPpbfQiisA0', 'aespa 에스파 ''도깨비불 (Illusion)'' (교차편집 Stage Mix)', DATE_SUB(NOW(), INTERVAL 4 DAY), DATE_SUB(NOW(), INTERVAL 4 DAY), DATE_SUB(NOW(), INTERVAL 4 DAY));
+
+insert into video_info (iframe_src, title, upload_date, modified_date, created_date)
+values ('https://www.youtube.com/embed/9bZkp7q19f0', 'VIVIZ(비비지) - MANIAC | 매니악 | 수트댄스 | Suit Dance | Performance | 4K', DATE_SUB(NOW(), INTERVAL 3 DAY), DATE_SUB(NOW(), INTERVAL 3 DAY), DATE_SUB(NOW(), INTERVAL 3 DAY));
+
