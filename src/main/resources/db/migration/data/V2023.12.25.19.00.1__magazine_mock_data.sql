ed696346b04578f9a62284ff23c7efe5977535ad
inhyeok <alzpqm@naver.com>
2023-12-25 19:30:18 +0900

    feat: add mock data

---
+++ src/main/resources/db/migration/data/V2023.12.25.19.00.1__magazine_mock_data.sql
@@ -0,0 +1,27 @@
+insert into magazine (volume_number, publish_date, volume_cover_link, file_link, modified_date, created_date)
+values (78, '2023-02-01', 'https://picsum.photos/1080/494', 'https://d73cu3lr763ke.cloudfront.net/magazine/SNU-78.pdf', NOW(), NOW());
+
+insert into magazine (volume_number, publish_date, volume_cover_link, file_link, modified_date, created_date)
+values (77, '2022-07-01', 'https://picsum.photos/1080/494', 'https://d73cu3lr763ke.cloudfront.net/magazine/SNU-77.pdf', NOW(), NOW());
+
+insert into magazine (volume_number, publish_date, volume_cover_link, file_link, modified_date, created_date)
+values (76, '2022-02-01', 'https://picsum.photos/1080/494', 'https://d73cu3lr763ke.cloudfront.net/magazine/SNU-76.pdf', NOW(), NOW());
+
+insert into magazine (volume_number, publish_date, volume_cover_link, file_link, modified_date, created_date)
+values (75, '2021-07-01', 'https://picsum.photos/1080/494', 'https://d73cu3lr763ke.cloudfront.net/magazine/SNU-75.pdf', NOW(), NOW());
+
+insert into magazine (volume_number, publish_date, volume_cover_link, file_link, modified_date, created_date)
+values (74, '2021-03-01', 'https://picsum.photos/1080/494', 'https://d73cu3lr763ke.cloudfront.net/magazine/SNU-74.pdf', NOW(), NOW());
+
+insert into magazine (volume_number, publish_date, volume_cover_link, file_link, modified_date, created_date)
+values (73, '2020-09-01', 'https://picsum.photos/1080/494', 'https://d73cu3lr763ke.cloudfront.net/magazine/SNU-73.pdf', NOW(), NOW());
+
+insert into magazine (volume_number, publish_date, volume_cover_link, file_link, modified_date, created_date)
+values (72, '2020-04-01', 'https://picsum.photos/1080/494', 'https://d73cu3lr763ke.cloudfront.net/magazine/SNU-72.pdf', NOW(), NOW());
+
+insert into magazine (volume_number, publish_date, volume_cover_link, file_link, modified_date, created_date)
+values (71, '2019-11-01', 'https://picsum.photos/1080/494', 'https://d73cu3lr763ke.cloudfront.net/magazine/SNU-71.pdf', NOW(), NOW());
+
+insert into magazine (volume_number, publish_date, volume_cover_link, file_link, modified_date, created_date)
+values (70, '2019-05-01', 'https://picsum.photos/1080/494', 'https://d73cu3lr763ke.cloudfront.net/magazine/SNU-70.pdf', NOW(), NOW());
+
