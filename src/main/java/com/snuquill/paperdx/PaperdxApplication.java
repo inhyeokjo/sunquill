39489605d310755c3461faec2ced252aab38b029
inhyeok <alzpqm@naver.com>
2024-04-12 01:55:00 +0900

    feat: set timezone

--- src/main/java/com/snuquill/paperdx/PaperdxApplication.java
+++ src/main/java/com/snuquill/paperdx/PaperdxApplication.java
@@ -1,10 +1,19 @@
 package com.snuquill.paperdx;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class PaperdxApplication {
+
+	@PostConstruct
+	public void stated() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaperdxApplication.class, args);
