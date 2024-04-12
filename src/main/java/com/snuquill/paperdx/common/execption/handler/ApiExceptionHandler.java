f3887adde1694ffb5e59a500b260f8215d8ef416
inhyeok <alzpqm@naver.com>
2024-04-12 13:31:01 +0900

    fix: return ExceptionHandler 404 error

--- src/main/java/com/snuquill/paperdx/common/execption/handler/ApiExceptionHandler.java
+++ src/main/java/com/snuquill/paperdx/common/execption/handler/ApiExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.snuquill.paperdx.common.execption.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 public class ApiExceptionHandler {
 
 	@ExceptionHandler(Throwable.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public String baseControllerAdvice() {
 		return "404";
 	}
