efc017d4f64f4096e06375d04a96a6a2f4a9ee4b
inhyeok <alzpqm@naver.com>
2024-09-26 07:26:41 +0900

    feat: article Detail 조회 기능 구현

---
+++ src/main/java/com/snuquill/paperdx/biz/article/ui/ArticleDetailRestController.java
@@ -0,0 +1,26 @@
+package com.snuquill.paperdx.biz.article.ui;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.snuquill.paperdx.biz.article.application.ArticleDetailDto;
+import com.snuquill.paperdx.biz.article.application.ArticleDetailService;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/article")
+@RequiredArgsConstructor
+@Validated
+public class ArticleDetailRestController {
+	private final ArticleDetailService articleDetailService;
+
+	@GetMapping("/{articleId}")
+	public ArticleDetailDto getArticleDetail(@NotNull @PathVariable Long articleId) {
+		return articleDetailService.getArticleDetail(articleId);
+	}
+}
