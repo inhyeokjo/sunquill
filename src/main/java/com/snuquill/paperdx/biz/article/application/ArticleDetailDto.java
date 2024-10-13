9fdd41febd340195ba43b7e10352df7a96052c90
inhyeok <alzpqm@naver.com>
2024-10-13 15:34:37 +0900

    feat: json ignore titleString

--- src/main/java/com/snuquill/paperdx/biz/article/application/ArticleDetailDto.java
+++ src/main/java/com/snuquill/paperdx/biz/article/application/ArticleDetailDto.java
@@ -2,11 +2,13 @@ package com.snuquill.paperdx.biz.article.application;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.snuquill.paperdx.biz.article.domain.Author;
 import com.snuquill.paperdx.biz.article.domain.Category;
 
 public record ArticleDetailDto(String pictureUrl, Category category, List<String> title, List<String> contents, Author author) {
 
+	@JsonIgnore
 	public String getTitleString() {
 		return String.join("\n", title);
 	}
